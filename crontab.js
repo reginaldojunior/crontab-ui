de70fe5a56beff94796bf4a197905e6a238eed6a
Reginaldo Junior <76regi@gmail.com>
2016-07-01 10:21:12 -0300

    Update crontab.js

--- crontab.js
+++ crontab.js
@@ -42,7 +42,7 @@ exports.remove = function(_id){
 	db.remove({_id: _id}, {});
 }
 exports.crontabs = function(callback){
-	console.log(db);
+	console.log(db.find({}));
 	db.find({}).exec(function(err, docs){
 		console.log(docs);
 		for(var i=0; i<docs.length; i++){
